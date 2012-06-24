be42e5f1ffcd09d77a09ee30a0b0b6872375b7a6
Joel Carranza <joel.carranza@gmail.com>
2012-06-24 16:16:46 -0500

    fix tests

--- tests.py
+++ tests.py
@@ -2,13 +2,14 @@ import random
 import geocoder
 import unittest
 import textdirections
+import otp
 
 # These are some basic tests of the system - collapsed into a single file for 
 # convience
 
 class OtpTest(unittest.TestCase):
   def test_plan(self):
-    print plan((29.976132,-90.080115),(29.949958,-90.109984),'BIKE')
+    print otp.plan((29.976132,-90.080115),(29.949958,-90.109984),'BIKE')
 
 class TextDirectionsTest(unittest.TestCase):
   def test_directions(self):
