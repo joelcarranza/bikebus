857dd09ba3f8963aa63fe3a37bdff58746dfaf72
Joel Carranza <joel.carranza@gmail.com>
2012-07-20 22:39:08 -0500

    add button which provides a from and to swap

--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,13 @@ function updateTimeField() {
   }
 }
 
+function swapFromTo() {
+  var fromv = $("#from-input").val();
+  var tov = $("#to-input").val();
+  $("#from-input").val(tov);
+  $("#to-input").val(fromv);
+}
+
 $(document).ready(function() {
   if($('.details').length > 1) {
     $('.details').hide();
@@ -14,13 +21,17 @@ $(document).ready(function() {
   $(".leg-header").click(function() {
     $('.details',this.parentNode).toggle();
   });
+  $("#action-swap").click(function(event) {
+    swapFromTo();
+    event.preventDefault();
+  });
   $("#edit-link").click(function(event) {
     $("#prompt").show();
     $("#result-head").hide();
     event.preventDefault();
-  })
+  });
   $("#timemode").change(function() {
     updateTimeField();
-  })
-  updateTimeField()
+  });
+  updateTimeField();
 });
